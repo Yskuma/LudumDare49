5f8dc071432a0230b1606a9e6ce92e4b6109c5ea
Mike <mike@livelyspark.com>
2021-10-02 23:41:49 +0100

    Added ActiveActions.

---
+++ core/src/com/livelyspark/ludumdare49/gameobj/ActiveActions.java
@@ -0,0 +1,8 @@
+package com.livelyspark.ludumdare49.gameobj;
+
+import com.livelyspark.ludumdare49.enums.Actions;
+import java.util.EnumSet;
+
+public class ActiveActions {
+    public EnumSet<Actions> activeActions = EnumSet.noneOf(Actions.class);
+}
