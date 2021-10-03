c7dd8dd27fced28f28b83f7383bfd38141218639
Mike <mike@livelyspark.com>
2021-10-03 22:19:35 +0100

    Turbine max speed threshold changed

--- core/src/com/livelyspark/ludumdare49/systems/TurbineSpeedSystem.java
+++ core/src/com/livelyspark/ludumdare49/systems/TurbineSpeedSystem.java
@@ -13,7 +13,7 @@ import com.livelyspark.ludumdare49.gameobj.PowerStation;
 public class TurbineSpeedSystem extends IteratingSystem {
 
     private PowerStation powerStation;
-    private static final int maxPower = 10000;
+    private static final int maxPower = 200;
 
     private ComponentMapper<AnimationComponent> am = ComponentMapper.getFor(AnimationComponent.class);
 
