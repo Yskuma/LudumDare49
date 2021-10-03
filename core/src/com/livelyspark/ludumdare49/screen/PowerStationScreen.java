36596068c0ed7513ee3f278df935fb2cf39c1fe7
Mike <mike@livelyspark.com>
2021-10-03 22:15:25 +0100

    Manual merge

--- core/src/com/livelyspark/ludumdare49/screen/PowerStationScreen.java
+++ core/src/com/livelyspark/ludumdare49/screen/PowerStationScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
